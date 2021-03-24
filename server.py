7c0ab168d655a947fd7d2782400f46a365441328
Carsten <carsten@cruncher.fritz.box>
2021-03-24 15:34:46 +0100

    It's working! 🥳

--- server.py
+++ server.py
@@ -1,4 +1,5 @@
-from flask import Flask
+import psycopg2
+from flask import Flask, request, Response
 app = Flask(__name__)
 
 @app.route('/')
@@ -8,4 +9,38 @@ def root():
 
 @app.route('/parking')
 def hello_park():
-    return 'Find a garage!'
+    lat = float(request.args.get("lat", 54))
+    lng = float(request.args.get("lng", 10))
+
+    # Connect to an existing database
+    conn = psycopg2.connect("""dbname=parking
+                               user=carsten""")
+
+    # Open a cursor to perform database operations
+    cur = conn.cursor()
+
+    # Query the database and obtain data as Python objects
+    cur.execute("""SELECT id, vejnavn, ST_AsGeoJSON(geom)
+                   FROM phus
+                   ORDER BY geom <-> 'SRID=4326;POINT(%s %s)'::geometry
+                   LIMIT 1;""", (lng, lat))
+    row = cur.fetchone()
+
+    output = '''{
+          "type": "Feature",
+          "properties": {
+              "id": '''+str(row[0])+''',
+              "streetname": "'''+row[1]+'''"},
+          "geometry": '''+row[2]+'''
+        }'''
+
+    cur.close()
+    conn.close()
+
+
+
+
+
+    return Response(response=output,
+                    status=200,
+                    mimetype="application/json")
